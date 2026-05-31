fbc576d7f9530ff248bb9c3063ce5f840e8a7d09
Eugenia <eu.zolotarenko@gmail.com>
2026-05-31 09:58:41 +0200

    Homework lesson 14. JUnitMethods and MethodsForTest methods concatWithSym , concatStrs

--- src/test/java/JUnitMethods.java
+++ src/test/java/JUnitMethods.java
@@ -41,10 +41,10 @@ public class JUnitMethods {
         //negative tests
 
         res = MethodsForTest.parsAndSum(" ", "45");
-        assertEquals(301707, res);
+        assertNull(res);
 
         res = MethodsForTest.parsAndSum("767", "");
-        assertEquals(301707, res);
+        assertNull(res);
 
         assertThrows(NumberFormatException.class,
                 ()->{MethodsForTest.parsAndSum("2147483648", "2147483647");}); //grater than max value
@@ -77,19 +77,77 @@ public class JUnitMethods {
                 ()->{MethodsForTest.parsAndSum("8,600","34");});
 
         res = MethodsForTest.parsAndSum(null, "-45");
-        assertEquals(301707, res);
+        assertNull(res);
 
         res = MethodsForTest.parsAndSum("-2147483648", null);
-        assertEquals(301707, res);
+        assertNull(res);
+
+
 
     }
 
     @Test public void sumElementsAray(){
+        //positive tests Lesson 29.05.2026
+
+        int[] arr = {-1,9,0, 24,-56,0, Integer.MAX_VALUE};
+        Integer res = MethodsForTest.sumElementsAray(arr, 0, 4);
+        assertTrue(-57==res);
+
+        res = MethodsForTest.sumElementsAray(arr, 0, 3);
+        assertTrue(23==res);
+
+        res = MethodsForTest.sumElementsAray(arr, 1, 3);
+        assertTrue(33==res);
+
+        res = MethodsForTest.sumElementsAray(arr, 2, 5);
+        assertTrue(0==res);
+
+        res = MethodsForTest.sumElementsAray(arr, 3, 1);
+        assertTrue(33==res);
+
+        res = MethodsForTest.sumElementsAray(arr, 1, 1);
+        assertTrue(18==res);
+
+        res = MethodsForTest.sumElementsAray(arr, 2, 3);
+        assertTrue(24==res);
+
+        res = MethodsForTest.sumElementsAray(arr, 4, 5);
+        assertTrue(-56==res);
+
+        //negative tests Lesson 29.05.2026
+        res = MethodsForTest.sumElementsAray(null, 4, 5);
+        assertNull(res);
+
+        res = MethodsForTest.sumElementsAray(arr,6,1);
+        assertNull(res);
+
+
+        int[] arr1 = {};
+
+        res = MethodsForTest.sumElementsAray(arr1, 4, 5);
+        assertNull(res);
+
+        res = MethodsForTest.sumElementsAray(arr, 4, 9);
+        assertNull(res);
+
+        res = MethodsForTest.sumElementsAray(arr, -4, 5);
+        assertNull(res);
+
+        res = MethodsForTest.sumElementsAray(arr, 9, 5);
+        assertNull(res);
+
+        res = MethodsForTest.sumElementsAray(arr, 4, arr.length);
+        assertNull(res);
+
+        res = MethodsForTest.sumElementsAray(arr, arr.length, 4);
+        assertNull(res);
+
+
         //positive tests
 
         int[] ar = {1,2,3,4,5};
 
-        int res = MethodsForTest.sumElementsAray(ar, 2, 3);
+        res = MethodsForTest.sumElementsAray(ar, 2, 3);
         assertEquals(7, res);
 
         int[] ar1 = {10, 20, 30, 40, 50};
@@ -109,23 +167,24 @@ public class JUnitMethods {
         assertEquals(-1, res);
 
         res= MethodsForTest.sumElementsAray(ar4, 0,2);
-        assertEquals(-2147483648, res);
-
+        assertNull(res);
+        res = MethodsForTest.sumElementsAray(arr, 4, 4);
+        assertTrue(-112==res);
 
 
         //negative tests
         int[] ar3 = {};
         res= MethodsForTest.sumElementsAray(ar3, 1,4);
-        assertEquals(301707, res);
+        assertNull(res);
 
         res= MethodsForTest.sumElementsAray(ar, -1,4);
-        assertEquals(301707, res);
+        assertNull(res);
 
         res= MethodsForTest.sumElementsAray(ar, 1,-4);
-        assertEquals(301707, res);
+        assertNull(res);
 
         res= MethodsForTest.sumElementsAray(ar, 1,5);
-        assertEquals(301707, res);
+        assertNull(res);
     }
 
     @Test public void concatStringArray (){
@@ -176,4 +235,175 @@ public class JUnitMethods {
         res = MethodsForTest.concatStringArray(ar4,0,2);
         assertNull(res);
     }
+
+    @Test public void concatWithSym(){
+        //positive test
+
+        String str1 = "hello";
+        String str2 = "money";
+        char sym = '#';
+
+        String res = MethodsForTest.concatWithSym(str1, str2, sym);
+        assertEquals("hello#money", res);
+
+        str1 = "Pneumonoultramicroscopicsilicovolcanoconiosis";
+        str2 = "illness";
+        sym = ':';
+        res = MethodsForTest.concatWithSym(str1, str2, sym);
+        assertEquals("Pneumonoultramicroscopicsilicovolcanoconiosis:illness", res);
+
+        str1 = "300000 km";
+        str2 = "sec is The Speed of Light in a Vacuum";
+        sym = '/';
+        res = MethodsForTest.concatWithSym(str1, str2, sym);
+        assertEquals("300000 km/sec is The Speed of Light in a Vacuum", res);
+
+        str1 = "A";
+        str2 = "z";
+        sym = '-';
+        res = MethodsForTest.concatWithSym(str1, str2, sym);
+        assertEquals("A-z", res);
+
+        str1 = "23";
+        str2 = "67";
+        sym = '+';
+        res = MethodsForTest.concatWithSym(str1, str2, sym);
+        assertEquals("23+67", res);
+
+        str1 = "";  //empty string
+        str2 = ""; //empty string
+        sym = ' '; //space
+        res = MethodsForTest.concatWithSym(str1, str2, sym);
+        assertEquals(" ", res);
+
+        str1 = " ";  //space
+        str2 = " "; //space
+        sym = ' '; //space
+        res = MethodsForTest.concatWithSym(str1, str2, sym);
+        assertEquals("   ", res);
+
+        str1 = "";  //empty string
+        str2 = ""; //empty string
+        sym = '?'; //space
+        res = MethodsForTest.concatWithSym(str1, str2, sym);
+        assertEquals("?", res);
+
+        str1 = "Centaurus";
+        str2 = "Nessus";
+        sym = ' '; //space
+        res = MethodsForTest.concatWithSym(str1, str2, sym);
+        assertEquals("Centaurus Nessus", res);
+
+        //negative tests
+
+        str1 = null;
+        str2 = "dgjdtyjdt";
+        sym = ']';
+        res = MethodsForTest.concatWithSym(str1, str2, sym);
+        assertNull(res);
+
+        str1 = "null";
+        str2 = null;
+        sym = 'F';
+        res = MethodsForTest.concatWithSym(str1, str2, sym);
+        assertNull(res);
+    }
+
+    @Test public void concatStsrs(){
+        //positive tests
+
+        String[] arStr1 = {"hello", "bye", "money", " " , null, "happy"}; //[4]space
+        String[] arStr2 = {"yes", "no", "", null, "maybe"}; //[2] empty
+
+        String res = MethodsForTest.concatStrs(arStr1, arStr2, 0,0);
+        assertEquals("helloyes", res);
+
+        res = MethodsForTest.concatStrs(arStr1, arStr2, 5,4);
+        assertEquals("happymaybe", res);
+
+        res = MethodsForTest.concatStrs(arStr1, arStr2, 2,1);
+        assertEquals("moneyno", res);
+
+        res = MethodsForTest.concatStrs(arStr1, arStr2, 1,2);
+        assertEquals("bye", res);
+
+        res = MethodsForTest.concatStrs(arStr1, arStr2, 3,1);
+        assertEquals(" no", res);
+
+        res = MethodsForTest.concatStrs(arStr1, arStr1, 2,5);
+        assertEquals("moneyhappy", res);
+
+        res = MethodsForTest.concatStrs(arStr2, arStr2, 1,1);
+        assertEquals("nono", res);
+
+        //negative tests
+
+        String[] arStr3 = {};
+        String[] arStr4 = null;
+
+        res = MethodsForTest.concatStrs(arStr3, arStr1, 0,1); //arStr3={};
+        assertNull(res);
+
+        res = MethodsForTest.concatStrs(arStr2, arStr3, 2,1); //arStr3={};
+        assertNull(res);
+
+        res = MethodsForTest.concatStrs(arStr4, arStr1, 2,1); //arStr4 = null;
+        assertNull(res);
+
+        res = MethodsForTest.concatStrs(arStr2, arStr4, 2,1); //arStr4 = null;
+        assertNull(res);
+
+        //res = MethodsForTest.concatStrs(arStr1, arStr2, -1,1);
+        //assertNull(res);
+
+        assertThrows(IndexOutOfBoundsException.class,
+                ()->{MethodsForTest.concatStrs(arStr1, arStr2, -1,1);});
+
+//        res = MethodsForTest.concatStrs(arStr2, arStr1, 2,-1);
+//        assertNull(res);
+        assertThrows(IndexOutOfBoundsException.class,
+                ()->{MethodsForTest.concatStrs(arStr1, arStr2, 2,-1);});
+
+        res = MethodsForTest.concatStrs(arStr1, arStr2, arStr1.length,1);
+        assertNull(res);
+
+        res = MethodsForTest.concatStrs(arStr1, arStr2, 2, arStr2.length);
+        assertNull(res);
+
+        res = MethodsForTest.concatStrs(arStr1, arStr2, 4, 0);//arStr1[4]==null
+        assertNull(res);
+
+        res = MethodsForTest.concatStrs(arStr1, arStr2, 2, 3); //arStr2[3]==null
+        assertNull(res);
+    }
 }
+
+
+/*
+Homework 29.05.2026
+1.
+public static String concatWithSum(String s1, String s2, char sym){}
+
+s1-> "hello"
+s2-> "money"
+sym-> '#'
+
+return "hello#money"
+
+in case any problem, return null;
+
+2.
+public static String concatStrs(String[] ar1, String[] ar2, int index1, int index2){}
+
+ar1->["hello"]["bye"]["money"]["happy"]
+
+ar2->["yes"]["no"]["maybe"]
+
+
+return ar1[index1]+ar2[index2];
+index1==2
+index2==1
+result: "moneyno"
+in case any problem, return null;
+
+ */
--- src/test/java/MethodsForTest.java
+++ src/test/java/MethodsForTest.java
@@ -4,11 +4,10 @@ public class MethodsForTest {
 
     //TDD Test Driven Development
 
-    public static int parsAndSum (String s1, String s2){
-
+    public static Integer parsAndSum (String s1, String s2){
 
         if(s1==null || s2==null|| s1.isBlank() || s2.isBlank())
-        return 301707;
+        return null;
 
         s1 = s1.strip();
         s2 = s2.strip();
@@ -19,11 +18,27 @@ public class MethodsForTest {
     public static Integer sumElementsAray (int[] array,
                                            int indexFirst,
                                            int indexSecond){
-        if (array.length == 0 || indexSecond<0 || indexFirst < 0
-           ||indexFirst>=array.length || indexSecond>=array.length)
-        return 301707;
+        if (array == null ||array.length == 0
+                || indexSecond<0 || indexFirst < 0
+                ||indexFirst>=array.length
+                || indexSecond>=array.length)
+            return null;
+
+        if (array[indexFirst] == Integer.MAX_VALUE && array[indexSecond]>0)
+            return null;
+
+        if (array[indexSecond] == Integer.MAX_VALUE && array[indexFirst]>0)
+            return null;
+
+        if (array[indexFirst] == Integer.MIN_VALUE && array[indexSecond]<0)
+            return null;
+
+        if (array[indexSecond] == Integer.MIN_VALUE && array[indexFirst]<0)
+            return null;
 
         return array[indexFirst] + array[indexSecond];
+
+
     }
 
     /*
@@ -37,14 +52,6 @@ public class MethodsForTest {
     public static String concatStringArray(String[] strArray,
                                            int indexFirst,
                                            int indexSecond){
-        /*
-        for(String s: strArray){
-            if (s == null){
-                return null;
-            }
-        }
-        /проверка всего массива на null
-         */
 
         if (strArray == null || strArray.length == 0
                 || indexSecond < 0 || indexFirst<0
@@ -65,7 +72,32 @@ public class MethodsForTest {
 
      */
 
+    public static String concatWithSym(String s1, String s2, char sym){
 
+        if(s1==null || s2 == null)
+        return null;
+
+        return s1+sym+s2;
+    }
+
+    public static String concatStrs(String[] ar1, String[] ar2, int index1, int index2){
+        if(ar1==null || ar2 == null //|| ar1.length == 0 || ar2.length == 0
+                // || index1 < 0 || index2 < 0                       // check with Throws:	IndexOutOfBoundsException
+          || index1 >= ar1.length || index2 >= ar2.length   //the order of conditions is important
+          || ar1[index1] == null || ar2[index2] == null
+        )                                                 // when || ar1.length == 0 || ar2.length == 0
+                                                         // place at the end of if- these conditions are always false
+        return null;                                     //because when ar1.length == 0, condition index1 >= ar1.length is true
+                                                        //so we can omit this conditions
+        return ar1[index1] + ar2[index2];
+    }
+
+/*
+which test is correct: check with Throws:IndexOutOfBoundsException
+or
+assertNull(res);
+which test should be written before method is implemented in case of TDDп
+ */
 
 
 }
